86bd363567621e8ef92266665cbf6b4de78252b6
silvianueno <silvia.nueno@alumni.esade.edu>
2025-11-03 18:55:06 +0100

    Update challenge1.py

--- challenge1.py
+++ challenge1.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 import glob
 
 # Load transactions data
@@ -16,3 +19,16 @@ score_files = glob.glob("scores_*.csv")
 
 # Combine all yearly score files into one DataFrame
 scores = pd.concat([pd.read_csv(f) for f in score_files], ignore_index=True)
+
+# --- DATA CLEANING ---
+# --- OUTLIER REMOVAL ---
+# We delete values outside the range of 1st and 99th percentile.
+
+
+# --- DATA EXPLORATION ---
+
+
+# --- FEATURE PREPARATION ---
+
+
+# --- CONCLUSION --- (poner tablas y comentarlas...)
